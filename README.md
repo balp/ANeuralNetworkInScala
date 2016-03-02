f81b9bbc20941bf1bb9059fe4995647f03f88261
Anders Arnholm <anders@arnholm.se>
2016-03-02 19:39:32 +0100

    Working Neural Network with backpropagation  in Scala

---
+++ README.md
@@ -0,0 +1,14 @@
+A Neural Network In Scala
+=========================
+
+**First** This project is about learning. I aim to learn two things
+from this project.
+
+* scala
+* neural networks
+
+The idea is based on i am trask's blogpost,
+[A Neural network in 11 rows of python](https://iamtrask.github.io/2015/07/12/basic-python-network/).
+But then  i knew python, and not scala.
+
+
--- src/main/scala/se/hiq/neural/NNWithBackPropagation.scala
+++ src/main/scala/se/hiq/neural/NNWithBackPropagation.scala
@@ -24,13 +24,13 @@ object NNWithBackPropagation extends App {
   var train = 0
   for( train <- 0 to 60000 ) {
     val l0 = X
-    val l1 = nonlin(l0 * syn0)
-    val l2 = nonlin(l1 * syn1)
+    val l1 = sigmoid(l0 * syn0)
+    val l2 = sigmoid(l1 * syn1)
     val l2_error = Y - l2
     if(train % 10000 == 0) println(mean(abs(l2_error)))
-    val l2_delta = l2_error :* nonlindelta(l2)
+    val l2_delta = l2_error :* sigmoid_derivate(l2)
     val l1_error  = l2_delta * syn1.t
-    val l1_delta  = l1_error :* nonlindelta(l1)
+    val l1_delta  = l1_error :* sigmoid_derivate(l1)
     syn1 :+= l1.t * l2_delta
     syn0 :+= l0.t * l1_delta
   }
@@ -40,10 +40,10 @@ object NNWithBackPropagation extends App {
   println(syn1)
 
 
-  def nonlindelta( X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+  def sigmoid_derivate(X : DenseMatrix[Double]) : DenseMatrix[Double] = {
     X :* ( X mapValues(1 - _))
   }
-  def nonlin(X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+  def sigmoid(X : DenseMatrix[Double]) : DenseMatrix[Double] = {
     1.0 :/ X.map( x => math.exp(-x) + 1)
   }
 }
