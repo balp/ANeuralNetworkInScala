284d24e38ba4b3379719b7e2b9d9168b8a217b8c
Anders Arnholm <anders.arnholm@hiq.se>
2016-03-02 18:49:47 +0100

    Kinda to scala... but well not working...

--- src/main/scala/se/hiq/neural/NetWorkOne.scala
+++ src/main/scala/se/hiq/neural/NetWorkOne.scala
@@ -19,15 +19,15 @@ object NetWorkOne extends App {
   for( train <- 1 to 500 ) {
     //    l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
     var l0 = X
-    var l1 = nonlin(l0 * syn0)
+    val l1 = nonlin(l0 * syn0)
     //    l2 = 1/(1+np.exp(-(np.dot(l1,syn1))))
-    var l2 = nonlin(l1 * syn1)
+    val l2 = nonlin(l1 * syn1)
 //    l2_delta = (y - l2)*(l2*(1-l2))
     var l2_error = Y - l2
     var l2_delta = nonlindelta(l2) * l2_error
 //    l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
-    var l1_error : DenseMatrix[Double] = l2_delta * syn1.t
-    var l1_delta : DenseMatrix[Double] = l1_error * nonlindelta(l1)
+    var l1_error  = l2_delta * syn1.t
+    var l1_delta  = l1_error * nonlindelta(l1)
 //    syn1 += l1.T.dot(l2_delta)
     syn1 :+= l1.t * l2_delta
 //    syn0 += X.T.dot(l1_delta)
@@ -39,17 +39,17 @@ object NetWorkOne extends App {
   println(syn1)
 
 
-  def nonlindelta( X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+  def nonlindelta( X : Vector[Double]) : Vector[Double] = {
     // x*(1-x)
     X * ( 1 - X)
   }
 
-  def nonlin(X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+  def nonlin(X : Vector[Double]) : Vector[Double] = {
     //1/(1+np.exp(-x))
-    var n : DenseMatrix[Double] = -X
-    var e : DenseMatrix[Double] = n.map( x => math.exp(x) )
-    var tmp : DenseMatrix[Double] = e + 1
-    var tmp2 : DenseMatrix[Double] = 1 / tmp
+    var n  = -X
+    var e  = n.map( x => math.exp(x) )
+    var tmp  = e + 1
+    var tmp2  = 1.0 / tmp
     return tmp2
   }
 }
