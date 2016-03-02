d7f2a2c0ebe3700c3c3b974f99d4e93ed873b8a5
Anders Arnholm <anders@arnholm.se>
2016-03-02 20:28:03 +0100

    Simple Gradient Decent optimizing

---
+++ src/main/scala/se/hiq/neural/NNGradientDecent2layer.scala
@@ -0,0 +1,32 @@
+package se.hiq.neural
+
+import breeze.linalg._
+import breeze.numerics._
+import breeze.stats.mean
+
+/**
+  * Created by balp on 2016-03-02.
+  */
+object NNGradientDecent2layer extends App {
+  val X = DenseMatrix( (0.0,1.0), (0.0,1.0), (1.0,0.0), (1.0,0.0) )
+  val Y = DenseMatrix( (0.0), (0.0), (1.0), (1.0) )
+  var synapse_0 = DenseMatrix.rand(2,1).mapValues( 2 * _ - 1)
+  var layer_1 : DenseMatrix[Double] = X
+  var train = 0
+  for( train <- 0 to 10000 ) {
+    val layer_0 = X
+    layer_1 = sigmoid(layer_0 * synapse_0)
+    val layer_1_error = layer_1 - Y
+    val layer_1_delta = layer_1_error :* sigmoid_output_to_derivative(layer_1)
+    synapse_0 :-= layer_0.t * layer_1_delta
+  }
+  println("Output after training")
+  println(layer_1)
+
+  def sigmoid( X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+    1.0 :/ X.map( x => 1 + math.exp(-x))
+  }
+  def sigmoid_output_to_derivative(X : DenseMatrix[Double]) : DenseMatrix[Double] = {
+    X :* ( X mapValues(1 - _))
+  }
+}
